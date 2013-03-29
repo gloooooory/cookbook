fd5deeaad0237cf8f23da415d009e13667968c1a
gloooooory <glo8ory@gmail.com>
2013-03-29 09:07:31 -0700

    Wrote recipe for injecting an expression into html

--- recipes/build.dart
+++ recipes/build.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 void main() {
   build(new Options().arguments, [
+      'web/webui/inject_expression/main.html',
       'web/webui/one_way_data_binding/main.html',
       'web/webui/two_way_data_binding/main.html',
       'web/webui/conditionals/main.html', 
