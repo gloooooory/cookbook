0511b471ecefe059a8a48eadf20a100164bac13c
gloooooory <glo8ory@gmail.com>
2013-03-30 19:29:35 -0700

    Added recipe to change page styles

--- recipes/build.dart
+++ recipes/build.dart
@@ -7,6 +7,7 @@ void main() {
       'web/webui/one_way_data_binding/main.html',
       'web/webui/two_way_data_binding/main.html',
       'web/webui/conditionals/main.html', 
+      'web/webui/change_style/main.html', 
       'web/webui/edit_form/main.html',
       'web/webui/loop/main.html',
       ]);
