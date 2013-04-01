b275c8ffede7baac75064231d83ea301ed8fbba7
gloooooory <glo8ory@gmail.com>
2013-04-01 06:35:45 -0700

    Updated build.dart

--- recipes/build.dart
+++ recipes/build.dart
@@ -8,8 +8,14 @@ void main() {
       'web/webui/two_way_data_binding/main.html',
       'web/webui/conditionals/main.html', 
       'web/webui/change_style/main.html', 
-      'web/webui/edit_form/main.html',
-      'web/webui/loop/main.html',
+
+      'web/webui/complexBinding/main.html',
+      'web/webui/complexBindingWithComposition/main.html',
+      'web/webui/complexBindingPartial/main.html',
+      'web/webui/complexBindingWithMap/main.html',
+      
+      'web/webui/loopUsingTable/main.html',
+      'web/webui/loop/main.html'
       ]);
 }
 
