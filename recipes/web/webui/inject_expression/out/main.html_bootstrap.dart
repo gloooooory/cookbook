c86a6bddd9d603536696c0fdb04618bad76061f2
gloooooory <glo8ory@gmail.com>
2013-04-01 06:30:46 -0700

    Tweaked inject_expression example

--- recipes/web/webui/inject_expression/out/main.html_bootstrap.dart
+++ recipes/web/webui/inject_expression/out/main.html_bootstrap.dart
@@ -1,7 +1,7 @@
 library bootstrap;
 
 import 'package:web_ui/watcher.dart' as watcher;
-import 'main.html.dart' as userMain;
+import 'main.dart' as userMain;
 
 main() {
   watcher.useObservers = false;
